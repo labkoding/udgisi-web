54f7a8a9ea167a403136d888ef72599aeea512ab
nofrets <opetstudio@gmail.com>
2023-06-13 07:42:55 +0800

    add url /welcome

--- src/main/java/id/co/labkoding/product/udgisi/controllers/IndexController.java
+++ src/main/java/id/co/labkoding/product/udgisi/controllers/IndexController.java
@@ -10,6 +10,7 @@ public class IndexController {
     @GetMapping(value= {
             "/home",
             "/",
+            "/welcome",
             "/Deliverynote",
             "/InvoiceScreen",
             "/ReceiptScreen",
